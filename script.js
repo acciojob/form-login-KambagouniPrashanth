4be2477e16d9b8c0504b383260327a52d7e534da
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-25 10:31:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const formele=document.getElementById("form1")
 function getFormvalue() {
     //Write your code here
+    const fname=formele.elements['fname'].value;
+    const lname=formele.elements['lname'].value;
+    alert(`${fname} ${lname}`)
+
 
 }
